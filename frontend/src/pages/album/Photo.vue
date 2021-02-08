81e3f1f37e707013591d8f63bc9539b21be07558
hanbeen <whgksqls13@naver.com>
2021-02-09 00:28:40 +0900

    mod : insert process of photo

--- frontend/src/pages/album/Photo.vue
+++ frontend/src/pages/album/Photo.vue
@@ -2,12 +2,18 @@
   <div class="row">
     <card class="col-sm-6 col-md-3">
       <div class="text-center">
-        <img src="@/assets/img/add.png" alt="PHOTO" v-on:click="insertPhoto" />
+        <img
+          src="@/assets/img/add.png"
+          alt="PHOTO"
+          v-on:click="fileopenclick"
+        />
         <input
           style="display: none"
           type="file"
           @change="onFileSelected"
           ref="fileInput"
+          id="upfile"
+          multiple="multiple"
         />
         <button @click="onUpload">Upload</button>
       </div>
@@ -122,61 +128,7 @@
         />
       </div>
     </card>
-
-    <card class="col-sm-6 col-md-3">
-      <div class="text-center">
-        <img src="@/assets/img/photo_basic.png" alt="PHOTO" />
-      </div>
-      <div class="text-center" style="margin-top: 25px">
-        <p><strong>SHARE YOUR PHOTOS</strong></p>
-        <p><mark>It'll be beautiful</mark></p>
-      </div>
-      <hr />
-      <div class="text-center">
-        <img
-          src="@/assets/img/report_basic.png"
-          alt="PHOTO"
-          style="margin-right: 25px; margin-bottom: 10px"
-        />
-        <img
-          src="@/assets/img/condol_basic.png"
-          alt="PHOTO"
-          style="margin-bottom: 10px"
-        />
-      </div>
-    </card>
-
-    <card class="col-sm-6 col-md-3">
-      <div class="text-center">
-        <img src="@/assets/img/photo_basic.png" alt="PHOTO" />
-      </div>
-      <div class="text-center" style="margin-top: 25px">
-        <p><strong>SHARE YOUR PHOTOS</strong></p>
-        <p><mark>It'll be beautiful</mark></p>
-      </div>
-      <hr />
-      <div class="text-center">
-        <img
-          src="@/assets/img/report_basic.png"
-          alt="PHOTO"
-          style="margin-right: 25px; margin-bottom: 10px"
-        />
-        <img
-          src="@/assets/img/condol_basic.png"
-          alt="PHOTO"
-          style="margin-bottom: 10px"
-        />
-      </div>
-    </card>
   </div>
-  <!-- <input
-    style="display:none"
-    type="file"
-    @change="onFileSelected"
-    ref="fileInput"
-  />
-  <button @click="fileopenclick">Pick File</button>
-  <button @click="onUpload">Upload</button> -->
 </template>
 
 <script>
@@ -194,26 +146,30 @@ export default {
       selectedFile: null,
       user: {
         email: "",
-      }
+      },
     };
   },
   methods: {
-    insertPhoto() {
-      alert("dddd");
+    fileopenclick() {
+      // alert("dddd");
       this.$refs.fileInput.click();
     },
-    fileopenclick() {},
     onFileSelected(event) {
       this.selectedFile = event.target.files[0];
     },
 
     onUpload() {
+      let user = this.user;
+
       const fd = new FormData();
       fd.append("image", this.selectedFile, this.selectedFile.name);
-      axios.post("${SERVER_URL}/gallary/upload", fd).then((res) => {
+      axios.post("${SERVER_URL}/gallary/upload", fd, user).then((res) => {
         console.log(res);
       });
     },
+  },
+  created() {
+    this.user.email = this.$store.getters["getEmail"];
   },
 };
 </script>
--- frontend/src/pages/user/Update.vue
+++ frontend/src/pages/user/Update.vue
@@ -65,7 +65,7 @@ export default {
         name: "",
         password: "",
         phone: ""
-      }
+      },
     };
   },
   methods: {

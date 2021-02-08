034b5e73585f7a6c5b481b89e3fdd964797c884e
hanbeen <whgksqls13@naver.com>
2021-02-09 00:12:28 +0900

    add : insert photo

--- frontend/src/pages/album/Photo.vue
+++ frontend/src/pages/album/Photo.vue
@@ -1,164 +1,220 @@
 <template>
-  <mdb-container>
-    <mdb-row>
-      <mdb-col sm="4">
-        <mdb-card wide>
-          <mdb-view hover cascade>
-            <a href="#!">
-              <mdb-mask flex-center waves overlay="white-slight"></mdb-mask>
-            </a>
-          </mdb-view>
-          <mdb-card-body class="text-center" cascade>
-            <mdb-card-title><strong>IMAGE</strong></mdb-card-title>
-            <h5 class="blue-text"><strong>I MISS YOU</strong></h5>
-            <mdb-card-text
-              >Sed ut perspiciatis unde omnis iste natus sit voluptatem
-              accusantium doloremque laudantium, totam rem
-              aperiam.</mdb-card-text
-            >
-            <div>
-              <b-button type="button" variant="dark">애도</b-button>
-              <mdb-card-text></mdb-card-text>
-              <b-button type="button" variant="dark">신고</b-button>
-            </div>
-          </mdb-card-body>
-        </mdb-card>
-      </mdb-col>
-      <mdb-col sm="4">
-        <mdb-card wide>
-          <mdb-view hover cascade>
-            <a href="#!">
-              <mdb-mask flex-center waves overlay="white-slight"></mdb-mask>
-            </a>
-          </mdb-view>
-          <mdb-card-body class="text-center" cascade>
-            <mdb-card-title><strong>IMAGE</strong></mdb-card-title>
-            <h5 class="blue-text"><strong>I MISS YOU</strong></h5>
-            <mdb-card-text
-              >Sed ut perspiciatis unde omnis iste natus sit voluptatem
-              accusantium doloremque laudantium, totam rem
-              aperiam.</mdb-card-text
-            >
-            <div>
-              <b-button type="button" variant="dark">애도</b-button>
-              <mdb-card-text></mdb-card-text>
-              <b-button type="button" variant="dark">신고</b-button>
-            </div>
-          </mdb-card-body>
-        </mdb-card> </mdb-col
-      ><mdb-col sm="4">
-        <mdb-card wide>
-          <mdb-view hover cascade>
-            <a href="#!">
-              <mdb-mask flex-center waves overlay="white-slight"></mdb-mask>
-            </a>
-          </mdb-view>
-          <mdb-card-body class="text-center" cascade>
-            <mdb-card-title><strong>IMAGE</strong></mdb-card-title>
-            <h5 class="blue-text"><strong>I MISS YOU</strong></h5>
-            <mdb-card-text
-              >Sed ut perspiciatis unde omnis iste natus sit voluptatem
-              accusantium doloremque laudantium, totam rem
-              aperiam.</mdb-card-text
-            >
-            <div>
-              <b-button type="button" variant="dark">애도</b-button>
-              <mdb-card-text></mdb-card-text>
-              <b-button type="button" variant="dark">신고</b-button>
-            </div>
-          </mdb-card-body>
-        </mdb-card>
-      </mdb-col>
-      <mdb-col sm="4">
-        <mdb-card wide>
-          <mdb-view hover cascade>
-            <a href="#!">
-              <mdb-mask flex-center waves overlay="white-slight"></mdb-mask>
-            </a>
-          </mdb-view>
-          <mdb-card-body class="text-center" cascade>
-            <mdb-card-title><strong>IMAGE</strong></mdb-card-title>
-            <h5 class="blue-text"><strong>I MISS YOU</strong></h5>
-            <mdb-card-text
-              >Sed ut perspiciatis unde omnis iste natus sit voluptatem
-              accusantium doloremque laudantium, totam rem
-              aperiam.</mdb-card-text
-            >
-            <div>
-              <b-button type="button" variant="dark">애도</b-button>
-              <mdb-card-text></mdb-card-text>
-              <b-button type="button" variant="dark">신고</b-button>
-            </div>
-          </mdb-card-body>
-        </mdb-card>
-      </mdb-col>
-      <mdb-col sm="4">
-        <mdb-card wide>
-          <mdb-view hover cascade>
-            <a href="#!">
-              <mdb-mask flex-center waves overlay="white-slight"></mdb-mask>
-            </a>
-          </mdb-view>
-          <mdb-card-body class="text-center" cascade>
-            <mdb-card-title><strong>IMAGE</strong></mdb-card-title>
-            <h5 class="blue-text"><strong>I MISS YOU</strong></h5>
-            <mdb-card-text
-              >Sed ut perspiciatis unde omnis iste natus sit voluptatem
-              accusantium doloremque laudantium, totam rem
-              aperiam.</mdb-card-text
-            >
-            <div>
-              <b-button type="button" variant="dark">애도</b-button>
-              <mdb-card-text></mdb-card-text>
-              <b-button type="button" variant="dark">신고</b-button>
-            </div>
-          </mdb-card-body>
-        </mdb-card>
-      </mdb-col>
-    </mdb-row>
-  </mdb-container>
+  <div class="row">
+    <card class="col-sm-6 col-md-3">
+      <div class="text-center">
+        <img src="@/assets/img/add.png" alt="PHOTO" v-on:click="insertPhoto" />
+        <input
+          style="display: none"
+          type="file"
+          @change="onFileSelected"
+          ref="fileInput"
+        />
+        <button @click="onUpload">Upload</button>
+      </div>
+      <div class="text-center" style="margin-top: 25px">
+        <p><strong>SHARE YOUR PHOTOS</strong></p>
+        <p><mark>It'll be beautiful</mark></p>
+      </div>
+      <hr />
+
+      <div class="text-center">
+        <img
+          src="@/assets/img/report_basic.png"
+          alt="PHOTO"
+          style="margin-right: 25px; margin-bottom: 10px"
+        />
+        <img
+          src="@/assets/img/condol_basic.png"
+          alt="PHOTO"
+          style="margin-bottom: 10px"
+        />
+      </div>
+    </card>
+
+    <card class="col-sm-6 col-md-3">
+      <div class="text-center">
+        <img src="@/assets/img/photo_basic.png" alt="PHOTO" />
+      </div>
+      <div class="text-center" style="margin-top: 25px">
+        <p><strong>SHARE YOUR PHOTOS</strong></p>
+        <p><mark>It'll be beautiful</mark></p>
+      </div>
+      <hr />
+      <div class="text-center">
+        <img
+          src="@/assets/img/report_basic.png"
+          alt="PHOTO"
+          style="margin-right: 25px; margin-bottom: 10px"
+        />
+        <img
+          src="@/assets/img/condol_basic.png"
+          alt="PHOTO"
+          style="margin-bottom: 10px"
+        />
+      </div>
+    </card>
+
+    <card class="col-sm-6 col-md-3">
+      <div class="text-center">
+        <img src="@/assets/img/photo_basic.png" alt="PHOTO" />
+      </div>
+      <div class="text-center" style="margin-top: 25px">
+        <p><strong>SHARE YOUR PHOTOS</strong></p>
+        <p><mark>It'll be beautiful</mark></p>
+      </div>
+      <hr />
+      <div class="text-center">
+        <img
+          src="@/assets/img/report_basic.png"
+          alt="PHOTO"
+          style="margin-right: 25px; margin-bottom: 10px"
+        />
+        <img
+          src="@/assets/img/condol_basic.png"
+          alt="PHOTO"
+          style="margin-bottom: 10px"
+        />
+      </div>
+    </card>
+
+    <card class="col-sm-6 col-md-3">
+      <div class="text-center">
+        <img src="@/assets/img/photo_basic.png" alt="PHOTO" />
+      </div>
+      <div class="text-center" style="margin-top: 25px">
+        <p><strong>SHARE YOUR PHOTOS</strong></p>
+        <p><mark>It'll be beautiful</mark></p>
+      </div>
+      <hr />
+      <div class="text-center">
+        <img
+          src="@/assets/img/report_basic.png"
+          alt="PHOTO"
+          style="margin-right: 25px; margin-bottom: 10px"
+        />
+        <img
+          src="@/assets/img/condol_basic.png"
+          alt="PHOTO"
+          style="margin-bottom: 10px"
+        />
+      </div>
+    </card>
+
+    <card class="col-sm-6 col-md-3">
+      <div class="text-center">
+        <img src="@/assets/img/photo_basic.png" alt="PHOTO" />
+      </div>
+      <div class="text-center" style="margin-top: 25px">
+        <p><strong>SHARE YOUR PHOTOS</strong></p>
+        <p><mark>It'll be beautiful</mark></p>
+      </div>
+      <hr />
+      <div class="text-center">
+        <img
+          src="@/assets/img/report_basic.png"
+          alt="PHOTO"
+          style="margin-right: 25px; margin-bottom: 10px"
+        />
+        <img
+          src="@/assets/img/condol_basic.png"
+          alt="PHOTO"
+          style="margin-bottom: 10px"
+        />
+      </div>
+    </card>
+
+    <card class="col-sm-6 col-md-3">
+      <div class="text-center">
+        <img src="@/assets/img/photo_basic.png" alt="PHOTO" />
+      </div>
+      <div class="text-center" style="margin-top: 25px">
+        <p><strong>SHARE YOUR PHOTOS</strong></p>
+        <p><mark>It'll be beautiful</mark></p>
+      </div>
+      <hr />
+      <div class="text-center">
+        <img
+          src="@/assets/img/report_basic.png"
+          alt="PHOTO"
+          style="margin-right: 25px; margin-bottom: 10px"
+        />
+        <img
+          src="@/assets/img/condol_basic.png"
+          alt="PHOTO"
+          style="margin-bottom: 10px"
+        />
+      </div>
+    </card>
+
+    <card class="col-sm-6 col-md-3">
+      <div class="text-center">
+        <img src="@/assets/img/photo_basic.png" alt="PHOTO" />
+      </div>
+      <div class="text-center" style="margin-top: 25px">
+        <p><strong>SHARE YOUR PHOTOS</strong></p>
+        <p><mark>It'll be beautiful</mark></p>
+      </div>
+      <hr />
+      <div class="text-center">
+        <img
+          src="@/assets/img/report_basic.png"
+          alt="PHOTO"
+          style="margin-right: 25px; margin-bottom: 10px"
+        />
+        <img
+          src="@/assets/img/condol_basic.png"
+          alt="PHOTO"
+          style="margin-bottom: 10px"
+        />
+      </div>
+    </card>
+  </div>
+  <!-- <input
+    style="display:none"
+    type="file"
+    @change="onFileSelected"
+    ref="fileInput"
+  />
+  <button @click="fileopenclick">Pick File</button>
+  <button @click="onUpload">Upload</button> -->
 </template>
 
 <script>
-import {
-  mdbContainer,
-  mdbRow,
-  mdbCol,
-  mdbCard,
-  mdbCardImage,
-  mdbCardHeader,
-  mdbCardBody,
-  mdbCardTitle,
-  mdbCardText,
-  mdbCardFooter,
-  mdbCardUp,
-  mdbCardAvatar,
-  mdbCardGroup,
-  mdbBtn,
-  mdbView,
-  mdbMask,
-  mdbIcon
-} from "mdbvue";
+import axios from "axios";
+import Card from "../../components/Cards/Card.vue";
+
 export default {
-  name: "CardProPage",
+  name: "Album",
   components: {
-    mdbContainer,
-    mdbRow,
-    mdbCol,
-    mdbCard,
-    mdbCardImage,
-    mdbCardHeader,
-    mdbCardBody,
-    mdbCardTitle,
-    mdbCardText,
-    mdbCardFooter,
-    mdbCardUp,
-    mdbCardAvatar,
-    mdbCardGroup,
-    mdbBtn,
-    mdbView,
-    mdbMask,
-    mdbIcon
-  }
+    Card,
+  },
+
+  data() {
+    return {
+      selectedFile: null,
+      user: {
+        email: "",
+      }
+    };
+  },
+  methods: {
+    insertPhoto() {
+      alert("dddd");
+      this.$refs.fileInput.click();
+    },
+    fileopenclick() {},
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0];
+    },
+
+    onUpload() {
+      const fd = new FormData();
+      fd.append("image", this.selectedFile, this.selectedFile.name);
+      axios.post("${SERVER_URL}/gallary/upload", fd).then((res) => {
+        console.log(res);
+      });
+    },
+  },
 };
 </script>
+<style></style>

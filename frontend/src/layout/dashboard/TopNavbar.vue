1b542e85de605d2f1cd137712bb391cb1af42c1a
김경연 <ghwoer12@naver.com>
2021-02-11 00:53:28 +0900

    ADD frontend

---
+++ frontend/src/layout/dashboard/TopNavbar.vue
@@ -0,0 +1,97 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">{{ routeName }}</a>
+      <button
+        class="navbar-toggler navbar-burger"
+        type="button"
+        @click="toggleSidebar"
+        :aria-expanded="$sidebar.showSidebar"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-bar"></span>
+        <span class="navbar-toggler-bar"></span>
+        <span class="navbar-toggler-bar"></span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav ml-auto">
+        <drop-down
+          class="nav-item"
+          title="5 Notifications"
+          title-classes="nav-link"
+          icon="ti-bell"
+        >
+          <a class="dropdown-item" href="#">편지에 대한 신고 2건입니다.</a>
+          <a class="dropdown-item" href="#">편지에 대한 신고 2건입니다.</a>
+        </drop-down>
+        <li class="nav-item">
+          <drop-down
+            class="nav-item"
+            title="회원관리"
+            title-classes="nav-link"
+            icon="ti-user"
+          >
+            <router-link to="/login" class="nav-link" v-if="getName === ''">
+              <p>
+                로그인
+              </p>
+            </router-link>
+            <router-link to="/login" class="nav-link" v-else>
+              <b-button variant="danger" @click="logout" class="col-md-6"
+                >로그아웃</b-button
+              >
+            </router-link>
+          </drop-down>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters([
+      "getAccessToken",
+      "getUserid",
+      "getName",
+      "getNumber",
+      "getAddress",
+      "getRoll"
+    ]),
+    routeName() {
+      const { name } = this.$route;
+      return this.capitalizeFirstLetter(name);
+    }
+  },
+  data() {
+    return {
+      activeNotifications: false
+    };
+  },
+  methods: {
+    logout: function() {
+      this.$store.dispatch("LOGOUT").then(() => {
+            this.$router.replace("/login");
+          });
+    },
+    capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+    toggleNotificationDropDown() {
+      this.activeNotifications = !this.activeNotifications;
+    },
+    closeDropDown() {
+      this.activeNotifications = false;
+    },
+    toggleSidebar() {
+      this.$sidebar.displaySidebar(!this.$sidebar.showSidebar);
+    },
+    hideSidebar() {
+      this.$sidebar.displaySidebar(false);
+    }
+  }
+};
+</script>
+<style></style>
